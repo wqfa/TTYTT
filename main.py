62bea17732e44c8fdda96dc20e8251e464a2d6f4
XF <118357460+wqfa@users.noreply.github.com>
2023-09-04 21:43:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
+import telebot,time,requests,re
+from telebot import types as t
 
-#This bot is developed by Hamoudi AL-Amir ★
-#Developer Channel | @MMMFi ✓.
+see = requests.Session()
 
-import telebot
-from telebot import types 
+der = telebot.TeleBot('6374219818:AAE0iUS7SWX5b6YHNEQbh_m13JI-Rwif-Ss')
 
-token = "6374219818:AAE0iUS7SWX5b6YHNEQbh_m13JI-Rwif-Ss"#توكنك
-bot = telebot.TeleBot(token)
-btn = types.InlineKeyboardButton(text='صانع الأساسي 𓅂', url = "https://t.me/C15CS")
-@bot.message_handler(commands=["start"])
-def start(message):
-	brok = types.InlineKeyboardMarkup()
-	brok.add(btn)
-	bot.reply_to(message,text='- أهلاً بك عزيزي  في بوت كتابة اسمك على قميص اللاعبين مع اختيار رقم الاعب ✅                                             • ارسل اسمك ثم الرقم مثل alamir:7 💯',reply_markup=brok)
-
-@bot.message_handler(content_types=['text'])
-def fo(message):
-	try:
-		msg = message.text
-		num = msg.split(':')[1]
-		name = msg.split(':')[0]
-		url = f'https://footballshirtmaker.com/en/spain/shirt-2021-II/number-{num}-{name}-001-square?v1200'
-		bot.send_photo(message.chat.id,url)
-	except:
-		bot.reply_to(message,'- حدث خطأ ⚠️                                                • يرجى ارسال الاسم والرقم على النحو الآتي الرقم:الاسم ، مثل  alamir:7❗️')
-
-bot.polling()
+@der.message_handler(commands=['start'])
+def start(mes):
+	d = t.InlineKeyboardButton(' dev 👨‍💻 ', url='t.me/C15CS')
+	de = t.InlineKeyboardMarkup(row_width=2);de.add(d)
+	na = mes.from_user.first_name
+	acc = der.send_message(mes.chat.id,text=f'Welcome {na} ,\nthis bot made by dergham\n\nsend username:password', reply_markup=de)
+	der.register_next_step_handler(acc,ax)
+def ax(mes):
+		ac = str(mes.text)
+		try:
+			user = ac.split(':')[0]
+			pasw = ac.split(':')[1]
+		except IndexError as error:ث
+			der.send_message(mes.chat.id,text='عذرا عزيزي 📛\nلا يمكنك الارسال على هذه النحو')
+			if not (':') in ac:
+				return
+		zo = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
+		zh = '1234567890'
+		import random
+		us="".join(random.choice(zo)for i in range(6))
+		ud="".join(random.choice(zo)for i in range(4))
+		udm="".join(random.choice(zh)for i in range(4))
+		ua=f'{us}/9.80 (Series 60; {us} {ud}/7.0.{udm}00/28.3445; U; en) Presto/2.8.119 Version/11.10'
+		see.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent": ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+		
+		p = see.get('https://m.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F').text
+	#print(p)
+		
+		dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p)).group(1),"uid":user,"flow":"login_no_pin","pass":pasw,"next":"https://developers.facebook.com/tools/debug/accesstoken/"}
+		
+		see.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent": ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+		
+		po = see.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,allow_redirects=False)
+		
+		kie = see.cookies.get_dict()
+		
+		if 'xs' and 'c_user' in kie:
+			koe = f"datr={kie['datr']};sb={kie['sb']};vpd=v1%3B633x360x2;locale=ar_AR;m_pixel_ratio=2;fr={kie['fr']};c_user={kie['c_user']};xs={kie['xs']};m_page_voice={kie['c_user']};wd=360x633;"
+			der.send_message(mes.chat.id,text=f'''<strong>
+✅ Good Cookie Facebook 
+---------------------------------------------
+<code>{koe}</code>
+---------------------------------------------
+@DevEviI - @C35CS </strong>''', parse_mode='html')
+		else:
+			der.send_message(mes.chat.id,text='عذرا عزيزي 📛\nخطا في تسجيل الدخول الرجاء إعادة المحاولة لطفاً')
+der.polling()
